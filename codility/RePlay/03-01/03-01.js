a1f219344c9a6c95e33c5321945be8ccaf3f5651
chany <lyhy0310@gmail.com>
2018-09-19 14:30:30 +0900

    [add] frogJump

--- codility/RePlay/03-01/03-01.js
+++ codility/RePlay/03-01/03-01.js
@@ -9,4 +9,10 @@ function solution(A) {
     if(Aobj[i]===undefined) return i;
   }
 }
+
+
+function solution(A) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  return [...new Array(A.length+2).keys()].concat(A).reduce((pre,cur)=>pre^cur);
+}
 console.log(solution([1,2]));
---
+++ codility/RePlay/03-02/03.02.js
@@ -0,0 +1,4 @@
+function solution(X, Y, D) {
+  return Math.ceil((Y-X)/D)
+  // write your code in JavaScript (Node.js 8.9.4)
+}
